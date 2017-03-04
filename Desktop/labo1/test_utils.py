e2dee3db45ea1afbafb299a66176aa9905b043be
Mohammad Zain <zain@hotmail.be>
2017-03-04 15:30:19 +0100

    Add more tests

--- Desktop/labo1/test_utils.py
+++ Desktop/labo1/test_utils.py
@@ -22,9 +22,9 @@ class TestUtils(unittest.TestCase):
         
     
     def test_integrate(self):
-        self.assertEqual(utils.integrate(1) ,(2 * x))
-        self.assertEqual(utils.integrate(2*x), (12 * x ** 2))
-        self.assertEqual(utils.integrate(4 * x ** 3),(42 * x ** 4)) 
+        self.assertEqual(utils.integrate('3*x ** 2 - 1', 0, 1), (0))
+        self.assertEqual(utils.integrate('2*x', 0, 1 ), (1))
+        self.assertEqual(utils.integrate("4 * x ** 3",0 ,1),(1))
         
 
 if __name__ == '__main__':
